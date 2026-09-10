acc94341d5e0de1a3922d66bfa5bf21a40eb299b
tkesh11 <yb605738@gmail.com>
2026-09-10 13:52:20 +0100

    Update page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -44,7 +44,7 @@ const token = cookieStore.get('novatrack_admin')?.value;
                 </div>
                 </div>
                 </div>
-                <section className="bg-brand-black rounded-x1 p-4">
+                <section className="bg-brand-black rounded-xl p-4">
           <h3 className="text-lg text-gold">Customer Feedback</h3>
           <div className="mt-3 space-y-3">
             {feedback.map((f: any) => (
